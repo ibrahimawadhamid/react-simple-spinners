91da2552f58c31c99259323e821d27733df15046
ibrahimawadhamid <ibrahim.a.hamid@gmail.com>
2018-12-14 23:44:15 +0200

    Initial Commit

---
+++ src/Bars/Bars.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './Bars.css';
+
+const Bars = () => (
+    <div className="loader">Loading...</div>
+);
+
+export default Bars
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Bars from './Bars/Bars';
+
+class LoadingAnimation extends Component {
+    render () {
+        return (
+            <Bars/>
+        );
+    }
+}
+
+export default LoadingAnimation;
